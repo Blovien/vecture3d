c11317eeec2345679d3625efd00aa540324d6fb6
Andrea Rossi <andrea.rossi03@pm.me>
2026-08-20 13:36:06 +0200

    fix(abi): preserve typedef declarations in manifest

--- ffi-java/src/main/public/v3_abi_manifest.h
+++ ffi-java/src/main/public/v3_abi_manifest.h
@@ -3,6 +3,6 @@
 #define V3_ABI_MANIFEST_H
 
 #define V3_ABI_VERSION UINT32_C(1)
-#define V3_ABI_SCHEMA_HASH UINT64_C(0x9ea771efbfc529bf)
+#define V3_ABI_SCHEMA_HASH UINT64_C(0xb091a9d2f24d73b8)
 
 #endif
